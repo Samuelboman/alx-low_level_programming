e20a265778a70ab0736400d9a031dd328729fd3c
Samuelboman <bomansamuel2021@gmail.com>
2023-10-03 16:09:54 -0700

    0x08. C - Recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -7,17 +7,17 @@
  * Return: 0
  */
 
-int wildcmp(char *s1, char *s2)
-{
-	if (*s1 == '\0')
-	{
-		if (*s2 != '\0' && *s2 == '*')
-		{
-			return (wildcmp(s1, s2 + 1));
-		}
-		return (*s2 == '\0');
-	}
+// Function prototype declaration
+int wildcmp(const char *s1, const char *s2);
 
+int main(void)
+{
+    int r;
+    r = wildcmp("main.c", "*.c");
+    // Rest of your code
+    return 0;
+}
+{
 	if (*s2 == '*')
 	{
 		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
