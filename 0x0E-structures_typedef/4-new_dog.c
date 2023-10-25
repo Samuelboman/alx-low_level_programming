087a05dc392d4585b3ba21ff711d740ce2027972
Samuelboman <bomansamuel2021@gmail.com>
2023-10-25 13:00:23 -0700

    update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -73,8 +73,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog->name = malloc(sizeof(char) * (len1 + 1));
 	if (dog->name == NULL)
 	{
-	free(dog);
-	return (NULL);
+		free(dog);
+		return (NULL);
 	}
 	dog->owner = malloc(sizeof(char) * (len2 + 1));
 	if (dog->owner == NULL)
