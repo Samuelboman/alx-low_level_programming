f415b63a5aace73929c09edede82f779eac0e790
Samuelboman <bomansamuel2021@gmail.com>
2023-10-25 15:14:41 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *temp = *head;
 	listint_t *current = NULL;
 	unsigned int i = 0;
-	
+
 	if (*head == NULL)
 	return (-1);
 
@@ -26,9 +26,9 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	while (i < index - 1)
 	{
 		if (!temp || !(temp->next))
-			return (-1);
-			temp = temp->next;
-			i++;
+		return (-1);
+		temp = temp->next;
+		i++;
 	}
 
 	current = temp->next;
